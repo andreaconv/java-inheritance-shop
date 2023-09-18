0a7d7347ac44ebbf97e10add10686fcadeb3042b
andreaconv <andrea2convertini@gmail.com>
2023-09-18 17:26:11 +0200

    [add] classe Cuffie

---
+++ src/org/lessons/java/shop/Cuffie.java
@@ -0,0 +1,40 @@
+package org.lessons.java.shop;
+
+public class Cuffie extends Prodotto {
+	
+	private String colore;
+	private boolean isCablate;
+	
+	public Cuffie(
+			String codice, String nome, String marca, float prezzo, int iva,
+			String colore, boolean isCablate) {
+		super (codice, nome, marca, prezzo, iva);
+		setColore(colore);
+		
+	}
+	
+	//COLORE
+	public String getColore() {
+		return colore;
+	}
+	public void setColore(String colore) {
+		this.colore = colore;
+	}
+	
+	//ISCABLATE
+	public boolean isCablate() {
+		return isCablate;
+	}
+	public void setCablate(boolean isCablate) {
+		
+		this.isCablate = isCablate;
+	}
+	
+	
+	@Override
+	public String toString() {
+		return "prodotto: " + getNome() + " | marca: " + getMarca() + " | prezzo senza IVA: € " + getPrezzo() 
+		+ " | prezzo con IVA: € " + String.format("%.02f", prezzoFormattato())
+		+ " | colore: " + getColore() + " | wireless: " + (!isCablate() ? "Si" : "No");
+	}
+}
--- src/org/lessons/java/shop/Main.java
+++ src/org/lessons/java/shop/Main.java
@@ -6,11 +6,13 @@ public class Main {
 		
 		Prodotto prodotto1 = new Prodotto("15648", "telefono", "samsung", 2899, 22);
 		Smartphone telefono1 = new Smartphone("468295", "telefono", "samsung", 2899, 22, 526489489, 128);
-		Televisori televisore1 = new Televisori("15648", "telefono", "samsung", 2899, 22, 65, true);
+		Televisori televisore1 = new Televisori("15648", "televisore", "samsung", 2899, 22, 65, true);
+		Cuffie cuffie1 = new Cuffie("15648", "cuffie", "samsung", 255, 22, "bianche", true);
 		
 		System.out.println(prodotto1);
 		System.out.println(telefono1);
 		System.out.println(televisore1);
+		System.out.println(cuffie1);
 	}	
 	
 }
