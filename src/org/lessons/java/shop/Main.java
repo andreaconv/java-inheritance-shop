19b72ed962b8974d9fabc616d976a59ccd2a078e
andreaconv <andrea2convertini@gmail.com>
2023-09-18 16:51:24 +0200

    [refactoring] classe Prodotto in Main separato

--- src/org/lessons/java/shop/Main.java
+++ src/org/lessons/java/shop/Main.java
@@ -2,64 +2,11 @@ package org.lessons.java.shop;
 
 public class Main {
 	
-	private String codice;
-	private String nome;
-	private String marca;
-	private float  prezzo;
-	private int iva;
-	
-	public Main(String codice, String name, String marca, float prezzo, int iva) {
+	public static void main(String[] args) {
 		
-		this.codice = codice;
-		setNome(name);
-		setMarca(marca);
-		setPrezzo(prezzo);
-		setIva(iva);
+		Prodotto prodotto1 = new Prodotto("15648", "telefono", "samsung", 2899, 22);
 		
-	}
+		System.out.println(prodotto1);
+	}	
 	
-	//CODICE
-	public String getCodice0() {
-		return codice;
-	}
-//	public void setCodice(String codice) {
-//		this.codice = codice;
-//	}
-	
-	//NOME
-	public String getNome() {
-		return nome;
-	}
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-
-	//MARCA
-	public String getMarca() {
-		return marca;
-	}
-	public void setMarca(String marca) {
-		this.marca = marca;
-	}
-	
-	//PREZZO
-	public float getPrezzo() {
-		return prezzo;
-	}
-	public void setPrezzo(float prezzo) {
-		this.prezzo = prezzo;
-	}
-	
-	//IVA
-	public int getIva() {
-		return iva;
-	}
-	public void setIva(int iva) {
-		this.iva = iva;
-	}
-	
-	public float prezzoFormattato() {
-		return getPrezzo() + (getPrezzo() / 100 * getIva());
-
-	}
 }
---
+++ src/org/lessons/java/shop/Prodotto.java
@@ -0,0 +1,71 @@
+package org.lessons.java.shop;
+
+public class Prodotto {
+
+	private String codice;
+	private String nome;
+	private String marca;
+	private float prezzo;
+	private int iva;
+	
+	public Prodotto(String codice, String nome, String marca, float prezzo, int iva) {
+		
+		this.codice = codice;
+		setNome(nome);
+		setMarca(marca);
+		setPrezzo(prezzo);
+		setIva(iva);
+		
+	}
+	
+	//CODICE
+	public String getCodice() {
+		return codice;
+	}
+//	public void setCodice(String codice) {
+//		this.codice = codice;
+//	}
+	
+	//NOME
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	//MARCA
+	public String getMarca() {
+		return marca;
+	}
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+	
+	//PREZZO
+	public float getPrezzo() {
+		return prezzo;
+	}
+	public void setPrezzo(float prezzo) {
+		this.prezzo = prezzo;
+	}
+	
+	//IVA
+	public int getIva() {
+		return iva;
+	}
+	public void setIva(int iva) {
+		this.iva = iva;
+	}
+	
+	public float prezzoFormattato() {
+		return getPrezzo() + (getPrezzo() / 100 * getIva());
+
+	}
+	
+	@Override
+	public String toString() {
+		return "prodotto: " + getNome() + " | marca: " + getMarca() + " | prezzo senza IVA: € " + getPrezzo() 
+		+ " | prezzo con IVA: € " + String.format("%.02f", prezzoFormattato());
+	}
+}
