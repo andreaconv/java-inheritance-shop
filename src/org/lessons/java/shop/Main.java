fe85e30e7ee0fd2eaac335af8b6235fd82fe624b
andreaconv <andrea2convertini@gmail.com>
2023-09-18 16:57:31 +0200

    [add] classe Smartphone

--- src/org/lessons/java/shop/Main.java
+++ src/org/lessons/java/shop/Main.java
@@ -5,8 +5,10 @@ public class Main {
 	public static void main(String[] args) {
 		
 		Prodotto prodotto1 = new Prodotto("15648", "telefono", "samsung", 2899, 22);
+		Smartphone telefono1 = new Smartphone("468295", "telefono", "samsung", 2899, 22, 526489489, 128);
 		
 		System.out.println(prodotto1);
+		System.out.println(telefono1);
 	}	
 	
 }
--- src/org/lessons/java/shop/Smartphone.java
+++ src/org/lessons/java/shop/Smartphone.java
@@ -1,38 +1,38 @@
-//package org.lessons.java.shop;
-//
-//public class Smartphone extends Prodotto {
-//
-//	private int imei;
-//	private int memoria;
-//	
-//	public Smarphone(
-//			String codice, String nome, String marca, float prezzo, int iva,
-//			int imei, int memoria) {
-//		super (codice, nome, marca, prezzo, iva);
-//		setImei(imei);
-//		setMemoria(memoria);
-//	}
-//	
-//	//IMEI
-//	public int getImei() {
-//		return imei;
-//	}
-//	public void setImei(int imei) {
-//		this.imei = imei;
-//	}
-//	
-//	//MEMORIA
-//	public int getMemoria() {
-//		return memoria;
-//	}
-//	public void setMemoria(int memoria) {
-//		this.memoria = memoria;
-//	}
-//	
-//	@Override
-//	public String toString() {
-//		return "prodotto: " + getNome() + " | marca: " + getMarca() + " | prezzo senza IVA: € " + getPrezzo() 
-//		+ " | prezzo con IVA: € " + String.format("%.02f", prezzoFormattato())
-//		+ " | IMEI: " + getImei() + " | memoria" + getMemoria();
-//	}
-//}
+package org.lessons.java.shop;
+
+public class Smartphone extends Prodotto {
+
+	private int imei;
+	private int memoria;
+	
+	public Smartphone(
+			String codice, String nome, String marca, float prezzo, int iva,
+			int imei, int memoria) {
+		super (codice, nome, marca, prezzo, iva);
+		setImei(imei);
+		setMemoria(memoria);
+	}
+	
+	//IMEI
+	public int getImei() {
+		return imei;
+	}
+	public void setImei(int imei) {
+		this.imei = imei;
+	}
+	
+	//MEMORIA
+	public int getMemoria() {
+		return memoria;
+	}
+	public void setMemoria(int memoria) {
+		this.memoria = memoria;
+	}
+	
+	@Override
+	public String toString() {
+		return "prodotto: " + getNome() + " | marca: " + getMarca() + " | prezzo senza IVA: € " + getPrezzo() 
+		+ " | prezzo con IVA: € " + String.format("%.02f", prezzoFormattato())
+		+ " | IMEI: " + getImei() + " | memoria " + getMemoria() + " GB";
+	}
+}
