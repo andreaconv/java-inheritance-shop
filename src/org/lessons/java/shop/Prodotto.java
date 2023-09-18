060550db3b5aa01c9f169fe8181cd7ab124bc9e7
andreaconv <andrea2convertini@gmail.com>
2023-09-18 14:57:35 +0200

    [add] costruttore, getter e setter

--- src/org/lessons/java/shop/Prodotto.java
+++ src/org/lessons/java/shop/Prodotto.java
@@ -1,8 +1,60 @@
 package org.lessons.java.shop;
 
 public class Prodotto {
-	public static void main(String[] args) {
+	
+	private String codice;
+	private String nome;
+	private String marca;
+	private float  prezzo;
+	private int iva;
+	
+	public Prodotto(String codice, String name, String marca, float prezzo, int iva) {
+		
+		setCodice(codice);
+		setNome(name);
+		setMarca(marca);
+		setPrezzo(prezzo);
+		setIva(iva);
 		
 	}
+	
+	//CODICE
+	public String getCodice0() {
+		return codice;
+	}
+	public void setCodice(String codice) {
+		this.codice = codice;
+	}
+	
+	//NOME
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
 
+	//MARCA
+	public String getMarca() {
+		return marca;
+	}
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+	
+	//PREZZO
+	public float getPrezzo() {
+		return prezzo;
+	}
+	public void setPrezzo(float prezzo) {
+		this.prezzo = prezzo;
+	}
+	
+	//IVA
+	public int getIva() {
+		return iva;
+	}
+	public void setIva(int iva) {
+		this.iva = iva;
+	}
 }
